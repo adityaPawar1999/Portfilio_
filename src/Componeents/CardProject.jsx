a6d9e295a288deacad8a5259175a19b40744fbf4
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2023-10-02 23:05:28 +0530

    CardProject.jsx

--- src/Componeents/CardProject.jsx
+++ src/Componeents/CardProject.jsx
@@ -6,7 +6,6 @@ const CardProject = (props) => {
       
       <a href={props.link} target="_blank" rel="noopener noreferrer" className="protfilioLink">
     <div class="card portfilioCard">
-      <img class="card-img-top" src={props.imgSrc} alt="Card images cap"/>
       <div class="card-body">
         <h5 class="card-title titlePort">{props.title} :</h5>
         <p class="card-text descriptionPort">{props.description}</p>
