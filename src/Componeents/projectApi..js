6c40c19db25aaf75515a3015f0f65dd48d7f2aa6
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2023-10-02 23:01:29 +0530

    projectApi.js

--- src/Componeents/projectApi..js
+++ src/Componeents/projectApi..js
@@ -13,6 +13,12 @@ const projectApi = [
     },
     {
       id: 3,
+      title:"TODO APP (mern)",
+      description:"A Full MERN (MongoDB, Express.js, React, Node.js) Todo application represents a powerful and versatile tool for task management and organization. ",
+      link:"https://github.com/adityaPawar1999/TODOAPP",
+    },
+    {
+      id: 4,
       title:"Random Color Generator",
       description:"Discover endless design possibilities with our Random Color Generator web app. Effortlessly generate unexpected color palettes to enhance your creativity. Elevate your designs ",
       link:"https://github.com/adityaPawar1999/Random-Color-Generator",
